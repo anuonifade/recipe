1c610d9cc60b75379a38aa06576c14ab11b907ae
andela-aonifade <anuoluwapo.onifade@andela.com>
2019-08-03 03:30:30 +0100

    initialize contentful with space_id and access_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :initialize_contentful
+
+  private
+  def initialize_contentful
+    Contentful::Client.new(
+      space: ENV['SPACE_ID'],
+      access_token: ENV['ACCESS_TOKEN'],
+      dynamic_entries: :auto
+    )
+  end
 end
