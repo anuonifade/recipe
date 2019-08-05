818d0d201f0f2427e20190a4444c78ff032886c7
andela-aonifade <anuoluwapo.onifade@andela.com>
2019-08-05 16:32:17 +0100

    created controller to fetch each recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,10 @@
 class RecipesController < ApplicationController
   def index
+    @recipe_info = @contentful_client.entry(id: get_recipe_id[:id])
+  end
+
+  private
+  def get_recipe_id
+    params.permit(:id)
   end
 end
